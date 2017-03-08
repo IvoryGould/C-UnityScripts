57b8e425b2aa4546a01e52b124bb806e4d7e8e66
IvoryGould <ivory.gould@gmail.com>
2017-03-08 15:05:31 +1100

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    CursorLockMode wantedMode;
+
+    void Start() {
+
+        wantedMode = CursorLockMode.Locked;
+
+    }
+
+
+    void Update() {
+
+        SetCursorState();
+
+    }
+
+    void SetCursorState() {
+
+        Cursor.lockState = wantedMode;
+
+    }
+}
---
+++ RaycastIntact.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RaycastIntact : MonoBehaviour
+{
+
+    RaycastHit hit;
+    float range = 2;
+
+    public Text interactText;
+
+    // float nextFire;
+    //float fireRate = 0.3f;
+
+    void Start(){
+        interactText.enabled = false;
+    }
+
+
+    void Update() {
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 2;
+        Debug.DrawRay(transform.position, forward, Color.green);
+
+        if (Physics.Raycast(transform.position, forward, out hit, range)){
+            if (hit.collider.tag == "Chest"){
+                print(hit.transform.name);
+                interactText.enabled = true;
+            }
+        } else {
+            interactText.enabled = false;
+        }
+    }
+}
